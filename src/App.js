dfa0146846a35dd7165d2d299e7778e9769482b5
chinmay_gujar07 <chinmaygujar52@gmail.com>
2023-04-14 00:17:46 +0530

    Authentication plus fire and the Context for state management

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route,Routes,Outlet } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component'
-import SignIn from './routes/sign-in/sign-in.component';
+import Authentication from './routes/authentication/authentication.component';
 const Shop=()=>{
 
   return (
@@ -24,7 +24,7 @@ const App=()=>{
     <Route path='/' element={<Navigation/>}>
       <Route index element={<Home/>}/>
       <Route path='shop' element={<Shop/>}/>
-      <Route path='signIn' element={<SignIn/>}/>
+      <Route path='signIn' element={<Authentication/>}/>
     </Route>
   </Routes>
  );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
+
+import { UserProvider } from './contexts/user.context';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <UserProvider>
       <App />
+    </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
