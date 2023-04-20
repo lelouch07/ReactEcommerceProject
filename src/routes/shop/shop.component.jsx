ed370269d24c67e8a2f485033f5d81db3162b730
chinmay_gujar07 <chinmaygujar52@gmail.com>
2023-04-20 11:35:24 +0530

    final commit

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,13 @@
 import {Routes,Route} from 'react';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../../components/category/category.component';
 import './shop.styles.scss'
+
 const Shop=()=>{
     return(
         // <Routes>
         //     <Route index element={<CategoriesPreview/>}/>
+        //     <Route  path=":category" element={<Category/>} />
         // </Routes>
         <CategoriesPreview/>
 
